8ba5c74f69d89d32913d439cfa59d19b120fb4d6
bABCDd <rlatkdrbs6845@gmail.com>
2025-08-25 12:17:38 +0900

    Initial commit

---
+++ Assets/Scripts/NumberUnit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberUnit : MonoBehaviour
+{
+
+    public int number;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
